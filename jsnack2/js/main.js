662c4a647dc5371579b847249a8683b092392ac0
Simonedambra <simonedambra130102@gmail.com>
2024-05-03 12:03:02 +0200

    [upd] space

--- jsnack2/js/main.js
+++ jsnack2/js/main.js
@@ -5,8 +5,8 @@ let primaParola = prompt('INSERISCI LA PRIMA PAROLA');
 let secondaParola = prompt('INSERISCI LA SECONDA PAROLA');
 
 if (primaParola.length > secondaParola.length) {
-    console.log(secondaParola + primaParola)
+    console.log(secondaParola + ' ' + primaParola)
 }
 else if (primaParola.length < secondaParola.length) {
-    console.log(primaParola + secondaParola)
+    console.log(primaParola + ' ' + secondaParola)
 }
